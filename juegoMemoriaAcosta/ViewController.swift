694dacbae15e8c34c80990eafe7b7a311f1e0123
fake <JuanAcosta@fake.local>
2016-02-07 21:57:53 -0600

    Initial Commit

---
+++ juegoMemoriaAcosta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juegoMemoriaAcosta
+//
+//  Created by fake on 2/7/16.
+//  Copyright © 2016 Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
